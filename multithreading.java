cb100d070e296166acd2425dca8ca52bee170c84
vini104 <125295527+vini104@users.noreply.github.com>
2023-03-03 21:43:03 +0530

    Update multithreading.java
    Signed-off-by: vini104 <125295527+vini104@users.noreply.github.com>

--- multithreading.java
+++ multithreading.java
@@ -1,1 +1,39 @@
+import java.util.concurrent.*;
+
+public class SubstringReverse {
+    public static String reverse(String str, int start, int end) {
+    StringBuilder sb = new StringBuilder(str.substring(start, end));
+    return sb.reverse().toString();
+  }
+
+  public static void main(String[] args) throws Exception {
+    String str = "Hello World!";
+    int start = 0;
+    int end = str.length();
+    int numThreads = Runtime.getRuntime().availableProcessors();
+
+    ExecutorService executor = Executors.newFixedThreadPool(numThreads);
+    CompletionService<String> completionService = new ExecutorCompletionService<>(executor);
+
+    int segmentLength = (end - start) / numThreads;
+
+    
+    for (int i = 0; i < numThreads; i++) {
+      final int segmentStart = start + i * segmentLength;
+      final int segmentEnd = (i == numThreads - 1) ? end : segmentStart + segmentLength;
+
+      completionService.submit(() -> reverse(str, segmentStart, segmentEnd));
+    }
+
+    
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < numThreads; i++) {
+      sb.append(completionService.take().get());
+    }
+
+    executor.shutdown();
+
+    System.out.println(sb.reverse().toString());
+  }
+}
 
